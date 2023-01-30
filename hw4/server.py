3f4e35a2f1af21fab3ab37750ba43651dd9cade3
kejiantong <tong.ke@northeastern.edu>
2023-01-30 01:45:36 -0800

    finish hw4

--- hw4/server.py
+++ hw4/server.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_title(url):
-    headers = {'User-Agent': 'Chrome/109'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/99 Safari/537'}
     page = requests.get(url, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
     title = soup.find('h1').get_text()
@@ -21,7 +21,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print(f"Connected by {addr}") 
         data = conn.recv(1024) 
-        url = data.decode('utf-8')
+        url = data.decode('utf-8').strip()
         title = get_title(url)
         conn.sendall(title.encode('utf-8'))
 
